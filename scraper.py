9f9d92ac6c565ed10f2d60822f1302a3b1f94603
Luca Muscat <luca.muscat.19@um.edu.mt>
2020-03-11 22:38:50 +0100

    Cleaned up code

--- scraper.py
+++ scraper.py
@@ -1,5 +1,3 @@
-import requests_html
-
 from requests_html import HTMLSession
 session = HTMLSession()
 url = "https://thenurseswithoutborders.org/COVID-19/index.rinj"
@@ -8,4 +6,5 @@ r = session.get(url)
 table = r.html.find('#myTable > tbody > tr')
 malta = [tr.find("td") for tr in table if "Malta" in tr.text][0]
 malta_text = [tr.text for tr in malta]
-print("Country: {}\n Cases: {}\n Deaths: {}\n Recoveries: {}".format(*malta_text))
+print("Country: {}\n Cases: {}\n Deaths: {}\n Recoveries: {}"
+      .format(*malta_text))
