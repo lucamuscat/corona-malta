9ce276eb2a953d31015f190b0fc71bdbc481655c
Luca Muscat <luca.muscat.19@um.edu.mt>
2020-03-11 22:30:06 +0100

    Finished scraper and requirments

---
+++ scraper.py
@@ -0,0 +1,11 @@
+import requests_html
+
+from requests_html import HTMLSession
+session = HTMLSession()
+url = "https://thenurseswithoutborders.org/COVID-19/index.rinj"
+r = session.get(url)
+
+table = r.html.find('#myTable > tbody > tr')
+malta = [tr.find("td") for tr in table if "Malta" in tr.text][0]
+malta_text = [tr.text for tr in malta]
+print("Country: {}\n Cases: {}\n Deaths: {}\n Recoveries: {}".format(*malta_text))
